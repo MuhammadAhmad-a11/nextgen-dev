84200ab60f7f26f04237df364b756474e733c50b
Muhammad Ahmad <muhammadahmadjaved83@gmail.com>
2026-04-07 23:03:24 +0500

    Initial html login pages

---
+++ jscript.js
@@ -0,0 +1,12 @@
+//Let                   Var(never use)                 Const
+
+//Let           BlockScope       Const
+
+let userId = 23443
+
+{
+    console.log(userId)   
+}
+
+//Global scope             And        BlockScope
+
