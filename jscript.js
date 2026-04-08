c58c2a0cda051871371f0108e71e36ffaaf33f60
Muhammad Ahmad <muhammadahmadjaved83@gmail.com>
2026-04-08 23:44:05 +0500

    moreChanges

--- jscript.js
+++ jscript.js
@@ -1,12 +1,69 @@
-//Let                   Var(never use)                 Const
+// // // //Let                   Var(never use)                 Const
 
-//Let           BlockScope       Const
+// // // //Let           BlockScope       Const
 
-let userId = 23443
+// // // let userId = 23443
 
-{
-    console.log(userId)   
-}
+// // // {
+// // //     console.log(userId)   
+// // // }
 
-//Global scope             And        BlockScope
+// // // //Global scope             And        BlockScope
+//***************************Conversion*************************** */
+// // // let score = "23abc";
+
+// // // console.log(typeof(score));
+
+// // // let inScore = Number(score);
+
+// // // console.log(typeof(inScore));
+// // // console.log(inScore);
+
+// // let a = "Ahmad"
+
+// // console.log(a)
+
+// // let course ="Hitesh"
+
+// // console.log(course)
+
+// // let isNumber = Boolean(course)
+// // console.log(isNumber)
+// // //Why we convert the data types of our variables in javascript Because the data conversion is must 
+// // //For some calculation like when ever q=a customer enters the input he enters in string form and we have to calculate it
+// // //And convert it into the numbers .
+
+// let userId = "Ahmad 141m"
+
+// //convert
+
+// let usersId = Number(userId)
+
+// console.log(typeof(usersId))
+// //What ever the input was in string.
+
+// let userName = "Ahmad"
+// let userPersonal = Boolean(userName)
+// console.log(userPersonal);
+
+// let score = 33
+
+// console.log(typeof (score))
+
+// let valueInNumber = String(score)
+// console.log( valueInNumber)
+// console.log(typeof valueInNumber)
+
+/*****************Operation********** */
+let value = 3
+let negValue = -value
+console.log(negValue)
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)
+//console.log(2%2)
+//console.log(2/2)
+
+//Basic operations we use in our daily life!
 
